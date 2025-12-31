7d2d67f4b15f936911d80fef93f12e6a634fb29c
henkvanhouten <henkjvanhouten@gmail.com>
2025-12-31 17:04:39 +0100

    Added Streamlit for web app

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-print("Welcome to the tip calculator!\n")
-bill = float(input("What was the total bill? "))
-tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
-group = int(input("How many people to split the bill? "))
+import streamlit as st
 
-calculated_tip = bill * (tip / 100)
-total_bill = bill + calculated_tip
-bill_per_person = round(total_bill / group, 2)
+st.title("Bill Calculator")
 
-print(f"Each person should pay: EUR {bill_per_person}")
+with st.form(key="bill_calculator_form"):
+    bill = st.number_input("What was the total bill? EUR ", min_value=0.0, step=1.0)
+    tip = st.number_input("How much tip would you like to give? 10, 12, or 15? ", min_value=0, step=1)
+    group = st.number_input("How many people to split the bill? ", min_value=1, step=1)
+
+    submit_button = st.form_submit_button(label="Calculate")
+
+if submit_button:
+    calculated_tip = bill * (tip / 100)
+    total_bill = bill + calculated_tip
+    bill_per_person = round(total_bill / group, 2)
+
+    st.success(f"Each person should pay: EUR {bill_per_person}")
